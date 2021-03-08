5643d7ff5b48083583552ef04cf3f6991e5162dc
solda <solda@khu.ac.kr>
2021-03-08 23:03:21 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+class App extends React.Component {
+  state={
+    isLoading:true,
+    movies:[]
+  }
+
+  componentWillMount() {
+    setTimeOut(()=>{
+      this.setState({isLoading:false});
+    },6000);
+  }
+  
+  render(){
+    const{isLoading}=this.state;
+    return <div>{isLoading?"Loading...":"We are ready"}</div>
+  }
+}
+
+
 class App extends React.Component{
   state={
     count:0
