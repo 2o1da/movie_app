49b7a2b55d311aabf4136d1feab8c8a8ac992416
Solda <solda@khu.ac.kr>
2021-03-11 22:53:29 +0900

    updated from MAC

--- src/App.js
+++ src/App.js
@@ -1,113 +1,97 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
-class App extends React.Component {
+class App extends React.Component{
   state={
     isLoading:true,
     movies:[]
+  };
+
+  getMovies=async()=>{
+    const{
+      data:{
+        data:{movies}
+      }
+    }=await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({movies,isLoading:false});
+  };
+
+  componentDidMount(){
+    this.getMovies();
   }
 
-  componentWillMount() {
-    setTimeOut(()=>{
-      this.setState({isLoading:false});
-    },6000);
-  }
-  
   render(){
     const{isLoading}=this.state;
-    return <div>{isLoading?"Loading...":"We are ready"}</div>
+    return (
+       <div>
+         {isLoading ? "Loading..."
+         : movies.map(movie=>(
+           <Movie
+           key={movie.id}
+           id={movie.id}
+           year={movie.year}
+           title={movie.title}
+           summary={movie.summary}
+           poster={movie.medium_cover_image}
+           />
+         ))}
+         </div>
+    );
   }
-}
-
-
+ }
+ 
 class App extends React.Component{
   state={
     count:0
   };
-
   add=()=>{
     this.setState(current=>({count:current.count+1}));
-    //console.log("add");
   };
-
   minus=()=>{
     this.setState(current=>({count:current.count-1}));
-    //console.log("minus");
-  }
-
-  componentDidMount() {
-    console.log("Component redered");
-  }
-  
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-
-  componentWillUnmount() {
-    console.log("Gooybye, cruel world");
-  }
-
+  };
   render(){
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>ADD</button>
-        <button onClick={this.minus}>MINUS</button>
-      </div>
-    )
-
+    <div>
+      <h1>The number is: {this.staste.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
   }
+}
 
+
+function Food({name,picture}){
+  //return <h3>I like {name}!</h3>;
+  return <div>
+    <h2>I like {name}.</h2>
+    <img src={picture}/>
+  </div>
 }
 
 const foodILike=[
   {
-    id:1, // react 내부에서 사용하기 위한 것
-    name2:"Kimchi",
-    images:"http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id:2,
-    name2:"Chicken",
-    images:"https://image.chosun.com/sitedata/image/201411/18/2014111803828_0.jpg"
+    name:"Kimchi",
+    images:"http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating:5
   }
 ]   
 
-/*
-function renderFood(dish){
-  console.log(dish);
-  return <Food name={dish.name2} picture={dish.images}/>
-}
-*/
 
-function Food({name,picture}){ // name, picture : property
-  return <div>
-    <h2>I like {name}.</h2>
-    <img src={picture} alt={name}/>
-  </div>
-  // HTML
-}
 
 function App() {
-  /*
-  return (
-  <div>
-    {console.log(foodILike.map(renderFood))}
-    {foodILike.map(renderFood)}
-  </div>
-  );
-  */
   return (
     <div>
       <h1>김다솔</h1>
       안녕하세요.
-      {foodILike.map(dish=> ( //javascript
-      <Food key={dish.id} name={dish.name2} picture={dish.images}/>
-      // key prop은 사용하지 않기 때문에 Food로 전달되지 않고 react 내부에서 사용하기 위한 것
+      {foodILike.map(dish=> (
+      <Food name={dish.name} picture={dish.images}/>
       ))} 
     </div>
   );
 }
- 
+
 export default App;
